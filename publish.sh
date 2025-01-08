928744232de0038412cd8b15f28a55554ef2bcec
bingxuelover <bingxuelover@126.com>
2025-01-08 15:00:47 +0800

    fix: 修正地址

--- publish.sh
+++ publish.sh
@@ -4,5 +4,5 @@ echo '请进行登录相关操作：'
 npm login # 登陆
 echo "-------publishing-------"
 npm publish # 发布
-npm config set registry=https://registry.npm.taobao.org # 设置为淘宝镜像
+npm config set registry=http://registry.npmmirror.com # 设置为淘宝镜像
 echo "发布完成"
